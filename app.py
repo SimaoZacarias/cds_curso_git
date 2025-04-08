91302d16191d700fff640551c9a63954a7ef22ea
Simao Zacarias <madalavalente@gmail.com>
2025-04-08 13:52:50 +0300

    Feita a edicao com o comando rebase

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ def load_data():
     
 def main():
 
-    df = load_data()
+    df_raw = load_data()
 
-    st.dataframe(df)
+    st.dataframe(df_raw)
 
 if __name__ == '_main_':
     main()
--- src/extraction.py
+++ src/extraction.py
@@ -2,3 +2,4 @@ import pandas as pd
 
 def load_data():
     return pd.read_csv('/Users/simaozacarias/Documents/data/02-Python/repos/cursoGit/projeto/data/bikes_completed.csv')
+
