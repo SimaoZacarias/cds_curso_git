346375c65af68834b54bfba03866a380d22448d0
Simao Zacarias <madalavalente@gmail.com>
2025-04-13 01:44:04 +0300

    Adicionado secao de Descricao dos Dados

--- app.py
+++ app.py
@@ -10,6 +10,34 @@ st.set_page_config(layout='wide')
 def load_data():
     return pd.read_csv('/Users/simaozacarias/Documents/data/02-Python/repos/cursoGit/projeto/data/exported/bikes.xlsx')
     
+def read_question13(df):
+
+
+    unico_dono = df1[df1['owner'] == '1st owner'].shape[0]
+    unico_dono
+
+    print(f'A base de dados possui {unico_dono} motos de um único dono')
+
+    df_grouped = df1.groupby('owner').agg(
+        qty = pd.NamedAgg('id', 'count')
+        ).sort_values('qty').reset_index()
+
+    ax = sns.barplot(
+            data=df_grouped,
+            x = 'owner',
+            y = 'qty'
+        )
+
+    ax.bar_label(ax.containers[0])
+
+    ax.set(
+            title = 'Quantidade de Motos por tipo de dono',
+            xlabel = 'Tipo de Dono',
+            ylabel = 'Quantidade de donos'
+        );
+
+    return None
+
 def main():
 
     df_raw = load_data()
@@ -18,4 +46,5 @@ def main():
 
 if __name__ == '_main_':
     main()
+
 
